656c4a173ca882276b3378c1740c029ffc0eb18b
Maciej Białkowski <maciekskc@gmail.com>
2023-06-04 21:45:26 +0200

    Adjust namespaces and dependencies

---
+++ Application/Repositories/BaseHttpRepository.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json;
+
+namespace Application.Repositories
+{
+    public abstract class BaseHttpRepository
+    {
+        private readonly HttpClient _client;
+        private readonly JsonSerializerOptions _options;
+
+        public BaseHttpRepository(HttpClient client)
+        {
+            _client = client;
+            _options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+        }
+
+        //protected async Task<TResponse> GetAsync(TResponse url)
+        //{
+        //    var response = await _client.GetAsync(url);
+        //    var content = await response.Content.ReadAsStringAsync();
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        throw new HttpRequestException(content);
+        //    }
+
+        //    var result = JsonSerializer.Deserialize<TResponse>(content, _options);
+        //    return result;
+        //}
+    }
+}
