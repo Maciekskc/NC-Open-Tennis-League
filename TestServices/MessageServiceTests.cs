188f2fca64b31b2f54a2fcb0b261622e609d714f
Maciek Bialkowski <48240229+Maciekskc@users.noreply.github.com>
2023-06-27 22:42:08 +0200

    Message service unit test implementation (#13)
    * Add IsActive field to teniss player and update data model
    
    * Add TennisPlayer service tests and first tests
    
    * 100% TennisPlayerSerivce code coverage. Introduce Bogus for data generation
    
    * Missing part of previous commit - bogus dependency
    
    * Remove redundant Blazor app setting
    
    * GameService Unit test implemented. 100% metchod code coverage
    
    * Cover GetMessages bussines logic with unit tests

---
+++ TestServices/MessageServiceTests.cs
@@ -0,0 +1,65 @@
+﻿using Infrastructure.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Persistance.Models;
+using TestHelpers;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using Communication.DTOs.Games;
+using Communication.DTOs.Message;
+
+namespace TestServices
+{
+    public class MessageServiceTests : ServiceTestBase<MessageService>
+    {
+        public MessageServiceTests():base()
+        {
+            _sut = new MessageService(_serviceCollection.BuildServiceProvider());
+        }
+
+        [Theory]
+        [InlineData(0)]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(1000)]
+        public async Task GetAllMessagesAsync_DifferentScenarios(int itemsCount)
+        {
+
+            // Attach
+            var matchResultMessages = GenerateMatchResultMessages(itemsCount);
+            var newChellangeMessages = GenerateNewChellangeMessages(itemsCount);
+
+            await _testContext.MatchResultMessages.AddRangeAsync(matchResultMessages);
+            await _testContext.NewChellangeMessages.AddRangeAsync(newChellangeMessages);
+            await _testContext.SaveChangesAsync();
+
+            // Act
+            var getAllGamesResult = await _sut.GetMessages();
+
+            // Assert
+            Assert.Equal(2*itemsCount, getAllGamesResult.Count());
+            Assert.All(getAllGamesResult, mrm => matchResultMessages.Any(p => p.Content == mrm.Content));
+            Assert.All(getAllGamesResult, ncm => newChellangeMessages.Any(p => p.Content == ncm.Content));
+        }
+
+        private static List<MatchResultMessage> GenerateMatchResultMessages(int count)
+        {
+            int currentPossition = 0;
+            var generator = new Faker<MatchResultMessage>()
+                .RuleFor(p => p.Content, f => f.Lorem.Text())
+                .RuleFor(p => p.Date, f => f.Date.Past(1))
+                .RuleFor(p=>  p.RelatedPositionUpdates, new List<LeaguePositions>());
+
+            return generator.Generate(count);
+        }
+
+        private static List<NewChellangeMessage> GenerateNewChellangeMessages(int count)
+        {
+            int currentPossition = 0;
+            var generator = new Faker<NewChellangeMessage>()
+                .RuleFor(p => p.Content, f => f.Lorem.Text())
+                .RuleFor(p => p.Date, f => f.Date.Past(1));
+
+            return generator.Generate(count);
+        }
+    }
+}
